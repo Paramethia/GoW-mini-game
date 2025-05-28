4512a5fb400633502d20c9439e9356f2967f1173
Kyrin <143742919+Paramethia@users.noreply.github.com>
2025-05-28 21:50:52 +0200

    Alpha 1.3 update

--- Functions.js
+++ Functions.js
@@ -2719,24 +2719,12 @@ function getEnemyAttackValue(level) {
 	return hit > 0 ? hit : 0;
 }
 
-let butle = false;
-
 function evade() {
-	if (!butle) {
-		butle = true;
-		evadeSound.play();
-		var attackIntervals = [ hopat, banshat, satat, minat, medat, cyclat, hercat, hermat, zeuat ];
-		var attackFunctions = [ hopliteShapa, bansheeShapa, satyrShapa, minotaurShapa, medusaShapa, cyclopsShapa, hermesShapa, herculesShapa, zuesShapa ];
-		clearInterval(attackIntervals[fighting];
-		setTimeout(() => {
-			attackFunctions[fighting]();
-			butle = false;
-		}, 700 );
-		if (fighting >= 6 ) {
-			text.innerText = "You evade the attack from " + enemies[fighting].name + ".";
-		} else {
-			text.innerText = "You evade the attack from the " + enemies[fighting].name;
-		}
+	evadeSound.play();
+	if (fighting >= 6 ) {
+		text.innerText = "You evade the attack from " + enemies[fighting].name + ".";
+	} else {
+		text.innerText = "You evade the attack from the " + enemies[fighting].name;
 	}
 }
 
